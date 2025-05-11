54e0ab2e44b00cee3a88a6fe1a2b05ec9d54b12b
starlei <starlei13@gmail.com>
2025-05-11 15:02:17 +0300

    add scripts 4 HW

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,7 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+  return title.toLowerCase().split(' ').join('-');
 }
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify('Arrays for beginners'));
+console.log(slugify('English for developer'));
+console.log(slugify('Ten secrets of JavaScript'));
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS'));
